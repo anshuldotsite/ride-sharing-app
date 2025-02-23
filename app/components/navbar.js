bf22f6f70632e139cc507016400d988708689a4e
Anshul Karande <anshulkarande20@gmail.com>
2025-02-23 03:02:20 -0500

    link tag fix

--- app/components/navbar.js
+++ app/components/navbar.js
@@ -27,12 +27,8 @@ export default function Navbar() {
           </>
         ) : (
           <>
-            <Link href="/login">
-              <a>Login</a>
-            </Link>
-            <Link href="/register">
-              <a>Register</a>
-            </Link>
+            <Link href="/login">Login</Link>
+            <Link href="/register">Register</Link>
           </>
         )}
         <DarkModeToggle />
